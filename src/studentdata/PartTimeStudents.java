8748c0ecd8a9cad40511257e7a291f4dccbc80a5
megha <megha.patel2@sheridancollege.ca>
2020-06-08 12:29:01 -0400

    first commit for parttimestudent branch

---
+++ src/studentdata/PartTimeStudents.java
@@ -0,0 +1,25 @@
+
+package studentdata;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class PartTimeStudents {
+
+    private int numOfhours;
+
+    /**
+     * @return the numOfhours
+     */
+    public int getNumOfhours() {
+        return numOfhours;
+    }
+
+    /**
+     * @param numOfhours the numOfhours to set
+     */
+    public void setNumOfhours(int numOfhours) {
+        this.numOfhours = numOfhours;
+    }
+}
