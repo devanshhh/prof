63f7200b7be23d615ee62aae321441663717a601
megha <megha.patel2@sheridancollege.ca>
2020-06-08 12:21:45 -0400

    my first commit

---
+++ src/studentdata/StudentData.java
@@ -0,0 +1,17 @@
+
+package studentdata;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class StudentData {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+
+}
